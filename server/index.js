cc9d59b84a1e7feb5630a3af1053a42c141d5131
bdk99 <bklein5@emich.edu>
2022-02-14 23:18:33 +0000

    Init Email Server

--- server/index.js
+++ server/index.js
@@ -5,12 +5,40 @@ const app = express();
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 const cors = require("cors");
-const {port, db} = require('./config.json');
+const {port, db, user, pass, from, subject} = require('./config.json');
+var nodemailer = require('nodemailer');
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(`${db}`);
+
+//Start of connection to email servers
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: (`${user}`),
+    pass: (`${pass}`)
+  }
+});
+
+var mailOptions = {
+  from: `${from}`,
+  to: `deliveritthere@gmail.com`,  //Set to email me (Brendan) for the time being until we get this setup
+  subject: `Password Reset`,
+  text: 'This is an outgoing email from the server application of our capstone project for COSC 481W located on the Gigabites domain.account.  Hope you enjoy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+//End of connection to email servers... And Email successfully sent
+
+
 
 app.get("/getUsers", (req, res) => {
   UserModel.find({}, (err, result) => {
