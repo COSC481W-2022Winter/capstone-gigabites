31fae40b53b82f9053dfd1c5a372f5a95dd8c9d9
bdk99 <bklein5@emich.edu>
2022-02-16 20:50:00 +0000

    Feat: Updates User Model as template for database

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,5 +1,6 @@
 //Credit to Code Base URL: https://www.youtube.com/watch?v=I7EDAR2GRVo
 
+//A model used to keep the document records in the database consistent
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
@@ -7,11 +8,23 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  age: {
-    type: Number,
+  username: {
+    type: String,
     required: true,
   },
-  username: {
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
     type: String,
     required: true,
   },
