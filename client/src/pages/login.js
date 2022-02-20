6e8431b1096c2a7622fffeaab63a5a1926112a70
anajid1 <anajid@emich.edu>
2022-02-20 21:10:04 +0000

    feat: Create login page

---
+++ client/src/pages/login.js
@@ -0,0 +1,40 @@
+import "../App.css";
+import React, { useState, useEffect } from "react";
+
+function login() {
+  return (
+    <form>
+      <div className="App" >
+        <div class="header">
+          <h1>Sign In</h1>
+          <h3>Welcome Back</h3>
+        </div>
+
+        <div className="border">
+          <table>
+            <tr>
+              <td>
+                {/*Username*/}
+                <label className="label">Username</label>
+                <input type="text" id='username'/>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                {/*Password*/}
+                <label className="label">Password</label>
+                <input type="text" id='password' />
+              </td>
+            </tr>
+          </table>
+
+          <button className="btn"> Submit </button>
+        </div>
+
+      </div>
+    </form>
+  );
+}
+
+export default login;
