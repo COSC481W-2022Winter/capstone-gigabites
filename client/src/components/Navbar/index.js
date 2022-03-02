8c0ff48a1081d9531bdafe764ebdcec77a4a4382
bdk99 <bklein5@emich.edu>
2022-03-02 22:47:41 +0000

    feat: user-session
    Added User Session
    Redirect to view-profile page on successful login
    Reconfigured login form validation
    Added stored variables when clicking the submit button via login screen
    Also added new npm package called 'react-client-session'
    
    Co-authored-by: RyanCornwall <RyanCornwall@users.noreply.github.com>

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
+import { ReactSession } from 'react-client-session';
+
+
 const navbar= () =>{
 	return (
-	<ul className="topnav">
-	  <li>
-		<Link to="/">Home</Link>
-	  </li>
-	  <li className ="right">
-		<Link to="/signup">Sign Up</Link>
-	  </li>
-      <li class ="right">
-          <Link to="/login">Login</Link>
-      </li>
-	  <li>
-		<Link to="/profile">Profile</Link>
-	  </li>
-	</ul>
+	<div>
+		{/* Shows the logged in session username above the NAVBAR for temporary testing and debugging purposes */}
+		<p>You are logged in as: {ReactSession.get('username')}</p>
+		<ul className="topnav">
+		<li>
+			<Link to="/">Home</Link>
+		</li>
+		<li className ="right">
+			<Link to="/signup">Sign Up</Link>
+		</li>
+		<li className ="right">
+			<Link to="/login">Login</Link>
+		</li>
+		<li>
+			<Link to="/profile">Profile</Link>
+		</li>
+		</ul>
+	</div>
 	);
   }
   export default navbar;
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,84 +1,100 @@
 import "../App.css";
-import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-const {passwordCompare, getPassStatus} = require('./config.json');
+import React from "react";
+import { ReactSession } from 'react-client-session';
+import { Link, Navigate } from 'react-router-dom';
+import axios from 'axios';
+const { passwordCompare } = require('./config.json');
 
-const axios = require('axios')
 
-function Login() {
+class Login extends React.Component {
+  constructor(val) {
+    super(val);
+    this.state = {username: '', password: '', redirect: false};
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  var passwordValidation = () => {
-    axios.post(`${passwordCompare}`, {
-      username,
-      password
-    })
-  };
-
-  useEffect(() => {
-    axios.get(`${getPassStatus}`).then((response) => {
-      console.log(response.data);
-      if(response.data === true) 
-        alert("login success");
-      else if(response.data === false)
-        alert ("Your username does not match this passcode. Try again");
-    });
-
-  }, []);
-
-  function validation() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    if(username==='' || password==='')
-    { return; }
-    else 
-    { 
-      passwordValidation(); 
-    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  return (
-    
-      <div className="App" >
-        <div className="header">
-          <h1>Login</h1>
-          <h3>Welcome Back</h3>
-        </div>
+  //Function executes whenever user changes the username textfield
+  handleUsernameChange(event) {
+    this.setState({username: event.target.value});
+  }
+  //Function executes whenever user changes the password textfield
+  handlePasswordChange(event) {
+   this.setState({password: event.target.value});
+  }
 
-        <div className="border">
-          <form>
-            <table>
-              <tbody>
+  //Runs whenever user clicks the submit button to validate entries
+  handleSubmit(event) {
+    if(this.state.username ==='' || this.state.password === '') {
+      alert("Missing username or password! Please try again.")
+      return;
+    }
+
+    //Compares password to the hashed one in the database
+    axios.post(`${passwordCompare}`, {
+      username: this.state.username,
+      password: this.state.password
+    }).then((res) => {
+      if(res.data === true){
+        ReactSession.set("username", this.state.username);
+        this.setState({redirect: true});
+      }
+      else  //Incorrect username/password information
+        alert ("Incorrect username or password!  Please try again.");
+    }).catch(() => {
+      console.log('Error alert!');
+    });
+    event.preventDefault();
+  }
+  render(){
+    //Alerts user of successful login, and redirects to user profile
+    if(this.state.redirect){
+      alert('Successfully logged in!');
+      return(<Navigate to="../profile" />);
+    }
+    else 
+    {
+      return (
+        <div className="App">
+          <div className="header">
+            <h1>Login</h1>
+            <h3>Welcome Back</h3>
+          </div>
+
+          <div className="border">
+            {/* On submit, validate username and password and compare user entry to records in database */}
+            <form onSubmit={this.handleSubmit}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      {/*Username*/}
+                      <label className="label">Username</label>
+                      <input type="text" id='username' value={this.state.username} onChange={this.handleUsernameChange}/>
+                    </td>
+                  </tr>
+                </tbody>
+
+                <tbody>
                 <tr>
                   <td>
-                    {/*Username*/}
-                    <label className="label">Username</label>
-                    <input type="text" id='username' onChange={(event) => {setUsername(event.target.value);}} required/>
-                  </td>
-                </tr>
-              </tbody>
-
-              <tbody>
-              <tr>
-                <td>
-                    {/*Password*/}
-                    <label className="label">Password</label>
-                    <input type="password" id='password' onChange={(event) => {setPassword(event.target.value);}} required/>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+                      {/*Password*/}
+                      <label className="label">Password</label>
+                      <input type="password" id='password' value={this.state.password} onChange={this.handlePasswordChange}/>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
           
-
-            <button onClick={validation} className="btn"> Login </button>
-            <Link to="/resetPassword" className="btn">Forgot Password</Link>
-          </form>
+              <button type="submit" value="Login" className="btn"> Login </button>
+              <Link to="/resetPassword" className="btn">Forgot Password</Link>
+            </form>
+          </div>
         </div>
-      </div>
-  );
+      )
+    }
+  }
 }
-
 export default Login;
