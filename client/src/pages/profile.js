08708f0c38f6d5c7677aa1dd1f7eb6aff0a0ff71
bdk99 <bklein5@emich.edu>
2022-03-15 00:16:37 +0000

    feat: Alerts when trying to view profile
    Adds alerts when a user is not logged in when viewing profile

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-  
-const Profile = () => {
-  return (
-    <div>
-      {/*Imports navbar to the top of the page*/}
-      <Navbar />
-      <h1>This is the profile page</h1>
-    </div>
-  );
+import { ReactSession } from 'react-client-session';
+import Alert from '@mui/material/Alert';
+class Profile extends React.Component {
+  render(){
+    return (
+      <div>
+        {/*Imports a material design style sheep using the Google API*/}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        
+        {/*Imports navbar to the top of the page*/}
+        <Navbar />
+        
+        {/*If the user is logged in, display the profile page*/}
+        {(ReactSession.get('username') !== undefined) &&
+          <h1>This is the profile page</h1>
+        }
+
+        {/*If the user is not logged in, display a simplistic profile page with an alert saying you must be logged in*/}
+        {(ReactSession.get('username') === undefined) &&
+          <div>
+            {/*Warning Material Design alert.  Please see
+            https://mui.com/components/alert/#main-content for more documentation/information*/}
+            
+            <Alert severity="error">You need to login to view profile!</Alert>
+          </div>
+        }
+      </div>
+    );
+  };
 };
   
 export default Profile;
