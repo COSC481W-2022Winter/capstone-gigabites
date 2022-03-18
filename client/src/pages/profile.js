8dcbae2f0be29a14395d9778992b582fd76dd95a
bdk99 <bklein5@emich.edu>
2022-03-18 23:17:01 +0000

    feat: Followers and following buttons
    * Added followers and following buttons to profile page
    * Added respective styles for both buttons to App.css

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import { ReactSession } from 'react-client-session';
+import {  Link } from "react-router-dom";
 import Alert from '@mui/material/Alert';
+import styled from "styled-components";
+
+const buttonStyle = styled.li;
+
 class Profile extends React.Component {
   render(){
     return (
       <div>
         {/*Imports a material design style sheep using the Google API*/}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-        
+     
         {/*Imports navbar to the top of the page*/}
         <Navbar />
         
         {/*If the user is logged in, display the profile page*/}
         {(ReactSession.get('username') !== undefined) &&
-          <h1>This is the profile page</h1>
+          <div>
+            <br/><br/>
+            <buttonStyle>
+              <Link to="/following" className="followingfollows">Following: 0</Link>
+            </buttonStyle>
+              <br/><br/><br/>
+            <buttonStyle>
+              <Link to="/followers" className="followingfollows">Followers: 0</Link>
+            </buttonStyle>
+            
+          </div>
         }
 
         {/*If the user is not logged in, display a simplistic profile page with an alert saying you must be logged in*/}
