acc39984dbff692a250e026045320303c642f4a2
anajid1 <anajid@emich.edu>
2022-04-16 23:44:40 +0000

    feat: Add frontend for recipe/user search
    The search is in a form since searching will redirect to a new page and
    this is an easy way to pass info to the new page.
    The search is in a table so the drop down for search type, input, and
    search button can be on the same line.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,9 +6,43 @@ const Home = () => {
 		<div className='Home-component'>		
 			{/*Imports navbar to the top of the page*/}
 			<Navbar />
-			<div className="borderHome">
-				<input className="homeSearch" type="text" />
-			</div>
+      <form>
+        <div className="borderHome">
+          <table className="centered">
+            <tbody>
+              <tr>
+                {/* Drop down for search type */}
+                <td>
+                  <select
+                    name="searchType" >
+                    <option defaultValue value="recipeName">Recipe Name</option>
+                    <option value="username">Username</option>
+                  </select> 
+                </td>
+
+                {/* Input for search */}
+                <td>
+                  <input 
+                  className="homeSearch"
+                  name="searchText" 
+                  type="text"
+                  required
+                  minLength="3"
+                  maxLength="100" />
+                </td>
+                
+                {/* Search button */}
+                <td>
+                  <input type="submit" value="Search"/>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          
+
+          
+        </div>
+      </form>
 		</div>
 	);
 };
